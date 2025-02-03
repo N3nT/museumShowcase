3ab89326599f345c551797470817db7e784cd5d4
N3nT <bar55v71@interia.pl>
2025-02-03 22:41:31 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Nav from './components/Nav';
+function App() {
+  return (
+    <div className="bg-stone-100 p-5 w-full h-screen overflow-hidden">
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+function Nav(){
+
+    return(
+        <nav>
+            <h1 className="font-bold">Art Institute of Chicago</h1>
+        </nav>
+    )
+}
+
+export default Nav;
