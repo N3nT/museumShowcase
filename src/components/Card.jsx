03abf3424ed3abd825514f4e293d40fb73bd4124
N3nT <bar55v71@interia.pl>
2025-02-05 23:00:52 +0100

    Fix image hidde

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,20 +3,16 @@ import { CircleHelp } from 'lucide-react';
 import data from "../data.json";
 function Card(){
 
-    const [backPanelOpacity, setBackPanelOpacity] = useState(0);
+    const [visibleInfo, setVisibleInfo] = useState(false);
     const handleQuestionClick = () => {
-        if(backPanelOpacity === 0){
-            setBackPanelOpacity(100);
-        }else{
-            setBackPanelOpacity(0);
-        }
-    }
-    console.log(data.pagination);
+        setVisibleInfo(!visibleInfo);
+    };
+
     return(
         <div className='relative max-w-[500px] m-4 overflow-hidden'>
-            <img src={`https://www.artic.edu/iiif/2/${data.data[0].image_id}/full/843,/0/default.jpg`} alt={data.data[0].title}/>
-            <CircleHelp className='absolute top-5 right-5 z-10' onClick={handleQuestionClick}/>
-            <div className={`w-full bg-stone-300 dark:bg-zinc-800 h-full absolute top-0 left-0 p-2 flex flex-col items-center justify-center opacity-${backPanelOpacity} transition-opacity duration-300 ease-in-out`}>
+            <img src={`https://www.artic.edu/iiif/2/${data.data[0].image_id}/full/843,/0/default.jpg`} alt={data.data[0].title} onClick={handleQuestionClick} className='cursor-pointer'/>
+            <CircleHelp className='absolute top-5 right-5 z-10 cursor-pointer' onClick={handleQuestionClick}/>
+            <div className={`w-full bg-stone-300 dark:bg-zinc-800 h-full absolute top-0 left-0 p-2 flex flex-col items-center justify-center ${visibleInfo ? "opacity-100" : "opacity-0" } transition-opacity duration-300 ease-in-out pointer-events-none`}>
                 <h3 className='font-bold text-xs'>{data.data[0].title}</h3>
                 <p className='text-xs'>{data.data[0].artist_title}</p>
                 <p className='text-center text-xs'>{data.data[0].short_description}</p>
