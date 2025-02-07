1e2942d985ebc653b36f1a5c24f9445bc4e04aad
N3nT <bar55v71@interia.pl>
2025-02-07 13:11:01 +0100

    Display html elements in short_description

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,7 +14,7 @@ const Card = (props) => {
             <CircleHelp className='absolute top-5 right-5 z-10 cursor-pointer w-7 h-7 sm:w-10 sm:h-10' onClick={handleQuestionClick}/>
             <div className={`w-full bg-stone-300 dark:bg-zinc-800 h-full absolute top-0 left-0 p-2 flex flex-col items-center justify-center ${visibleInfo ? "opacity-100" : "opacity-0" } transition-opacity duration-300 ease-in-out pointer-events-none text-xs sm:text-lg sm:p-5` }>
                 <h3 className='font-bold '>{props.title} - {props.artist_title}</h3>
-                <p className='text-center'>{props.short_description}</p>
+                <p className='text-center' dangerouslySetInnerHTML={{ __html: props.short_description }}></p>
                 <a href="" className='mt-10 p-5 bg-zinc-800 dark:bg-stone-300 text-white dark:text-black'>See more</a>
             </div>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
+import { Instagram, Github, Linkedin } from 'lucide-react';
+
 const Footer = () => {
 
     return(
         <div className="max-w-[1000px] p-5 flex justify-between w-full items-center">
             <div>
                 <p>Author: <a href="https://github.com/N3nT" target="_blank">N3nT</a></p>
-                <img src="https://imgs.search.brave.com/jeCEwgL2vjQ0tDXZSzBMtSa5NPVXeFfoBlmwtPvsKFE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/YnV5bWVhY29mZmVl/LmNvbS9idXR0b24t/YXBpLz90ZXh0PUJ1/eSUyMG1lJTIwYSUy/MGNvZmZlZSZlbW9q/aT0mc2x1Zz1yZWJl/bG9pZCZidXR0b25f/Y29sb3VyPUZGREQw/MCZmb250X2NvbG91/cj0wMDAwMDAmZm9u/dF9mYW1pbHk9Q29v/a2llJm91dGxpbmVf/Y29sb3VyPTAwMDAw/MCZjb2ZmZWVfY29s/b3VyPWZmZmZmZg" alt="" className="w-30"/>
+                <div className='flex w-[100px] justify-around'>
+                    <a href="#"><Instagram /></a>
+                    <a href="#"><Github /></a>
+                    <a href="#"><Linkedin /></a>
+                </div>
             </div>
             <p>All images are on under Creative Commons Zero (CC0)</p>
         </div>
