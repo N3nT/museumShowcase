0e3f81cce4f3e264756a9145326fd4c2c84e9c2f
N3nT <bar55v71@interia.pl>
2025-02-08 20:04:07 +0100

    H1 as link to Home.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,7 +16,6 @@ const Card = (props) => {
                     <CircleHelp className='absolute top-5 right-5 z-10 cursor-pointer w-7 h-7 sm:w-10 sm:h-10' onClick={handleQuestionClick}/>
                     <div className={`w-full bg-stone-300 dark:bg-zinc-800 h-full absolute top-0 left-0 p-2 flex flex-col items-center justify-center ${visibleInfo ? "opacity-100" : "opacity-0 cursor-pointer" } transition-opacity duration-300 ease-in-out text-xs sm:text-lg p-5 ` } onClick={handleQuestionClick}>
                         <h3 className='text-center'>{props.title} {props.artist_title === null ? ('') : (<>- <span className='font-bold'>{props.artist_title}</span> </>)} </h3>
-                        {/* <p className='text-center' dangerouslySetInnerHTML={{ __html: props.short_description }}></p> */}
                         <Link to={`/image/${props.id}`} className='mt-10 p-5 bg-zinc-800 hover:bg-zinc-950 dark:bg-stone-100 hover:dark:bg-stone-300 text-white dark:text-black'>See more
                         </Link>
                     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 import { SunMedium, Moon } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   
@@ -9,7 +10,9 @@ const Nav = () => {
 
     return(
         <nav className='flex justify-between items-center max-w-[1000px] w-full p-5'>
-            <h1 className="font-bold sm:text-xl">Art Institute of Chicago</h1>
+            <Link to={"/"}>
+              <h1 className="font-bold sm:text-xl">Art Institute of Chicago</h1>
+            </Link>
             <button onClick={toggleTheme}>{theme === 'light' ? (<Moon className='w-7 h-7 sm:w-9 sm:h-9 cursor-pointer'/>) : (<SunMedium className='w-7 h-7 sm:w-9 sm:h-9 cursor-pointer'/>)}</button>
         </nav>
     )
